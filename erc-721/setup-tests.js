56379807967132ccd56e61e3695d9885fe640f19
wisekim-eth <140994329+wisekim-eth@users.noreply.github.com>
2023-08-01 18:33:01 +0900

    Delete erc-721 directory

--- erc-721/setup-tests.js
+++
@@ -1,9 +0,0 @@
-const BN = web3.utils.BN;
-const chai = require("chai");
-
-chai.use(require("chai-bn")(BN));
-chai.use(require("chai-as-promised"));
-
-chai.config.includeStack = true;
-
-module.exports = chai;
